16689c9816a98cf3ea855926a17aa99beeb0c717
carlosprietobarron <carlosprietobarron@gmail.com>
2021-02-02 09:32:06 -0600

    FEATURE: Add store to redux project

--- src/component/App.js
+++ src/component/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import '../css/App.css';
 import BookForm from './BookForm';
 import BookList from './BookList';
+import rootReducer from '../reducers';
+
+const store = createStore(rootReducer);
 
 function App() {
   return (
-    <Provider>
+    <Provider store={store}>
       <div className="App">
-        <h1>Hello</h1>
         <BookList />
         <BookForm />
       </div>
