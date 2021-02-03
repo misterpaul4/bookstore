53f4c1d460f9e73b4b8c1a1fbe46ac6e3f6fe426
paul <ajuzepaul@gmail.com>
2021-02-03 18:13:58 +0200

    Move BookList and BookForm to the containers folder

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Book from './Book';
+import Book from '../component/Book';
 
 const BookList = ({ books: { books } }) => {
   const callBook = book => (<Book book={book} />);
